9d6be46a611ba18fe70e9dccc342a7c22aee6922
ericagerez <ericagerez63@gmail.com>
2021-04-20 02:05:16 -0300

    Creating structure

---
+++ unquibooking/src/test/java/ar/edu/unq/tip/unquibooking/UnquibookingApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.tip.unquibooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnquibookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
