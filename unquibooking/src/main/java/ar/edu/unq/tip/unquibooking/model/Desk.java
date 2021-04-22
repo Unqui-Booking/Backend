6dd64be22a1d52c9c9b8a892d0299aa90d052376
ericagerez <ericagerez63@gmail.com>
2021-04-22 01:58:10 -0300

    Add name for desks

--- unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/model/Desk.java
+++ unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/model/Desk.java
@@ -14,8 +14,11 @@ public class Desk {
 		@Column(unique=true, nullable=false)
 		private Long id;
 
-		@OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.REMOVE)
-		private List<Booking> bookings = new ArrayList<Booking>();
+		@Column(length=50, nullable = false, unique = true)
+		private String nameDesk;
+
+		//@OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.REMOVE)
+		//private List<Booking> bookings = new ArrayList<Booking>();
 
 
 		public Long getId() {
@@ -26,7 +29,13 @@ public class Desk {
 			this.id = id;
 		}
 
+		public String getName(){
+			return nameDesk;
+		}
 
+		public void setName(String nameDesk){
+			this.nameDesk = nameDesk;
+		}
 		
 		
 }
