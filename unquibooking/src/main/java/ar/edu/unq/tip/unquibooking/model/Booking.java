336debfa201329fedd605e0652c7cd4bb3401426
ericagerez <ericagerez63@gmail.com>
2021-04-23 16:41:16 -0300

    Booking model

--- unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/model/Booking.java
+++ unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/model/Booking.java
@@ -1,5 +1,59 @@
 package ar.edu.unq.tip.unquibooking.model;
 
+import java.time.LocalDate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "booking")
 public class Booking {
     
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(unique=true, nullable=false)
+    private Long id;
+
+    @ManyToOne
+    private Desk desk;
+
+    @Column
+    private LocalDate date;
+    
+    @Column
+    private Integer startTime;
+
+    @Column
+    private Integer endTime;
+
+    public void setDate(LocalDate date){
+        this.date = date;
+    }
+
+    public LocalDate getDate(){
+        return date;
+    }
+
+    public void setStartTime(Integer time){
+        this.startTime = time;
+    }
+
+    public Integer getStartTime(){
+        return this.startTime;
+    }
+
+    public void setEndTime(Integer time){
+        this.endTime = time;
+    }
+
+    public Integer getEndTime(){
+        return this.endTime;
+    }
+
+    public void setDesk(Desk desk){
+        this.desk = desk;
+    }
+
+    public Desk getDesk(){
+        return desk;
+    }
 }
