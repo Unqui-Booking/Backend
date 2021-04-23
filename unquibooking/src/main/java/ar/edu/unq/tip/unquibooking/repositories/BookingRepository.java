bd7529f4a69aa3f02308f555ace2ef2f4d291b3f
ericagerez <ericagerez63@gmail.com>
2021-04-23 17:14:32 -0300

    Booking service

--- unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/repositories/BookingRepository.java
+++ unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/repositories/BookingRepository.java
@@ -1,11 +1,13 @@
 package ar.edu.unq.tip.unquibooking.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ar.edu.unq.tip.unquibooking.model.Booking;
 
-public interface BookingRepository extends CrudRepository{
+public interface BookingRepository extends CrudRepository<Booking, Long>{
 
-    public abstract Booking findByDesk(Long idDesk);
+    public abstract Optional<Booking> findByDesk(Long idDesk);
     
 }
