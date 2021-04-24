71e884b2297051d22241fc437f78b21bd13a8475
ericagerez <ericagerez63@gmail.com>
2021-04-24 00:18:51 -0300

    Add annotation for repository booking

--- unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/repositories/BookingRepository.java
+++ unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/repositories/BookingRepository.java
@@ -3,9 +3,11 @@ package ar.edu.unq.tip.unquibooking.repositories;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import ar.edu.unq.tip.unquibooking.model.Booking;
 
+@Repository
 public interface BookingRepository extends CrudRepository<Booking, Long>{
 
     public abstract Optional<Booking> findByDesk(Long idDesk);
