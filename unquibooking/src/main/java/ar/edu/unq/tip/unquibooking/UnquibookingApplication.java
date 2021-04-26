345edd93bd0998b7541e5cc6a8d9c85105c096c0
ericagerez <ericagerez63@gmail.com>
2021-04-26 01:45:30 -0300

    Changing syntax error for archile main

--- unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/UnquibookingApplication.java
+++ unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/UnquibookingApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UnquibookingApplication {
 
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		SpringApplication.run(UnquibookingApplication.class, args);
 	}
 
