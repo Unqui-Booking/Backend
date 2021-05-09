662ef3032d16216cfc39ae222c97304e0f82929b
ericagerez <ericagerez63@gmail.com>
2021-05-09 17:38:58 -0300

    Model Desk: adding attributes area and maxmaxAmountSeats

--- unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/model/Desk.java
+++ unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/model/Desk.java
@@ -3,19 +3,33 @@ package ar.edu.unq.tip.unquibooking.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name= "desk")
+@Table(name="desk")
 public class Desk {
 
 	@Id
-	@GeneratedValue(strategy= GenerationType.IDENTITY)
-	@Column(unique=true, nullable=false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(unique=true, nullable=false, name="desk_id")
 	private Long id;
 
-	@Column(length=50, nullable = false, unique = true)
+	@Column(length=50, nullable=false, unique=true)
 	private String nameDesk;
 
+	@Column(nullable=false)
+	private Integer maxAmountSeats;
+	
+	@Column(length=50, nullable=false)
+	private String area;
+	
+	public Desk(String nameDesk, Integer maxAmountSeats, String area) {
+		this.nameDesk = nameDesk;
+		this.maxAmountSeats = maxAmountSeats;
+		this.area = area.toLowerCase();
+	}
+	
+	public Desk() { }
+
 	public Long getId() {
-		return this.id;
+		return id;
 	}
 
 	public void setId(Long id) {
@@ -29,6 +43,23 @@ public class Desk {
 	public void setNameDesk(String nameDesk){
 		this.nameDesk = nameDesk;
 	}
-		
+	
+	public Integer getMaxAmountSeats(){
+		return maxAmountSeats;
+	}
+
+	public void setMaxAmountSeats(Integer maxAmountSeats){
+		this.maxAmountSeats = maxAmountSeats;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+	
 	
 }
