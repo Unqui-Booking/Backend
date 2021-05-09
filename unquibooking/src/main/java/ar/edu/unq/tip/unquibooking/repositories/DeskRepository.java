e697997b1093078c78f0c6d62dd70e0189b7f096
ericagerez <ericagerez63@gmail.com>
2021-05-09 17:41:46 -0300

    Filter desks by area

--- unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/repositories/DeskRepository.java
+++ unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/repositories/DeskRepository.java
@@ -1,10 +1,15 @@
 package ar.edu.unq.tip.unquibooking.repositories;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 import ar.edu.unq.tip.unquibooking.model.Desk;
 
 @Repository
 public interface DeskRepository extends CrudRepository<Desk, Long> {
     
+	public abstract ArrayList<Desk> findByArea(String area);
+	
 }
