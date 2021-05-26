eb5dbe0de3c0f3efb87c1468d72cec9388080199
ericagerez <ericagerez63@gmail.com>
2021-05-26 01:39:25 -0300

    Exceptions for Seats

--- unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/services/SeatService.java
+++ unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/services/SeatService.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import ar.edu.unq.tip.unquibooking.dto.SeatDTO;
+import ar.edu.unq.tip.unquibooking.exception.DeskNotFoundException;
+import ar.edu.unq.tip.unquibooking.exception.SeatNotFoundException;
+import ar.edu.unq.tip.unquibooking.model.Desk;
 import ar.edu.unq.tip.unquibooking.model.Seat;
 import ar.edu.unq.tip.unquibooking.repositories.SeatRepository;
 
@@ -14,30 +16,40 @@ import ar.edu.unq.tip.unquibooking.repositories.SeatRepository;
 public class SeatService {
 	@Autowired
     SeatRepository seatRepository;
-
+	@Autowired
+	DeskService deskService;
+	
     public ArrayList<Seat> getAllSeats(){
         return (ArrayList<Seat>) seatRepository.findAll();
     }
-
-    public Seat saveSeat(Seat seat){
-        return seatRepository.save(seat);
+    
+    public Seat saveSeat(Seat seat) throws DeskNotFoundException{
+    	Long deskId = seat.getDesk().getId();
+    	Desk desk = deskService.getDesk(deskId);
+        Seat newSeat = seatRepository.save(seat);
+        newSeat.setDesk(desk);
+        return newSeat;
     }
 
-    public Seat getSeat(Long idSeat){
-        return seatRepository.findById(idSeat).get();
+    public Seat getSeat(Long idSeat) throws SeatNotFoundException{
+    	Optional<Seat> optionalSeat = seatRepository.findById(idSeat);
+    	if(!optionalSeat.isPresent()) {
+    		throw new SeatNotFoundException("Seat not found");
+    	}
+        return optionalSeat.get();
     }
 
-    public boolean deleteById(Long id){
-        try{
-            seatRepository.deleteById(id);
-            return true;
-        }
-        catch(Exception error){
-            return false;
-        }
+    public boolean deleteById(Long id) throws SeatNotFoundException{
+    	Optional<Seat> optionalSeat = seatRepository.findById(id);
+    	if(!optionalSeat.isPresent()) {
+    		throw new SeatNotFoundException("Seat not found");
+    	}
+    	optionalSeat.get().setDeleted(true);
+    	seatRepository.save(optionalSeat.get());
+        return optionalSeat.get().isDeleted();
     }
 
-    public ArrayList<Seat> getByDesk(Long desk){
+	public ArrayList<Seat> getByDesk(Long desk) {
         return seatRepository.findByDeskId(desk);
     }
 
