6c7941a0cc3efbaf7094f1a0096a57ec30a0937d
ericagerez <ericagerez63@gmail.com>
2021-05-27 23:12:11 -0300

    User controller

---
+++ unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/controller/UserController.java
@@ -0,0 +1,40 @@
+package ar.edu.unq.tip.unquibooking.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.edu.unq.tip.unquibooking.model.User;
+import ar.edu.unq.tip.unquibooking.services.UserService;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping()
+	public User saveUser(User user) {
+		return userService.saveUser(user);
+	}
+	
+	@GetMapping(path="/{id}")
+	public User getUSer(@PathVariable("id") Long id) {
+		return userService.getUSer(id);
+	}
+	
+	@GetMapping("/query")
+	public ArrayList<User> getUserByMail(@RequestParam("mail") String mail){
+		return userService.getUserByMail(mail);
+	}
+
+}
