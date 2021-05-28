8c2d9b68f3b2137bb581080f84f818b0d0f5889e
ericagerez <ericagerez63@gmail.com>
2021-05-27 22:38:47 -0300

    User repository

---
+++ unquibooking/src/main/java/ar/edu/unq/tip/unquibooking/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package ar.edu.unq.tip.unquibooking.repositories;
+
+import java.util.ArrayList;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import ar.edu.unq.tip.unquibooking.model.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{
+
+	public abstract ArrayList<User> findByMail(String mail);
+	
+}
